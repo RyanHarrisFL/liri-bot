9ef31a31ff8987403bc65c41462bf8a633ddfbf4
Rhino <ryanharrisfl@gmail.com>
2019-03-22 16:59:53 -0400

    Updated bands in town to include console logs

--- liri.js
+++ liri.js
@@ -1,20 +1,49 @@
 require("dotenv").config();
-
+var axios = require("axios");
 var fs = require("fs");
+var moment = require('moment');
 
 var keys = require("./keys.js");
 var spotify = new Spotify(keys.spotify);
 
 var requestType = process.argv[2];
-var requestValue = process.argv[3];
+var nodeArgs = process.argv;
 
-var axios = require("axios");
+var artists = "";
 
-var queryUrl = "https://rest.bandsintown.com/artists/" + requestType + "/events?app_id=codingbootcamp"
+switch (requestType) {
+  case "concert-this":
+    bandsInTown();
+    break;
+}
 
-axios.get(queryUrl).then(
-    function(response) {
-      console.log(response.data);
+//Bands In Town Function
+function bandsInTown() {
+  for (var i = 3; i < nodeArgs.length; i++) {
+    if (i > 3 && i < nodeArgs.length) {
+      artists = artists + "+" + nodeArgs[i];
+    } else {
+      artists += nodeArgs[i];
     }
-  );
-  
+  }
+
+  var queryUrl =
+    "https://rest.bandsintown.com/artists/" +
+    artists +
+    "/events?app_id=codingbootcamp";
+
+  axios.get(queryUrl).then(function(response) {
+    for (i = 0; i < response.data.length; i++){
+      console.log("Name of the venue: " + response.data[0].venue.name);
+      console.log("City: " + response.data[i].venue.city);
+      console.log("Date of the event: " + moment(response.data[i].datetime).format("L"));
+      }
+  });
+}
+
+
+function spotifyThisSong() {
+
+
+
+}
