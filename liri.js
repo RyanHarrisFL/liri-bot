3610f45800c486ffaa4e240f2fd553b7708ce834
Rhino <ryanharrisfl@gmail.com>
2019-03-21 20:22:14 -0400

    set up response for Bands in Town

--- liri.js
+++ liri.js
@@ -1,1 +1,20 @@
 require("dotenv").config();
+
+var fs = require("fs");
+
+var keys = require("./keys.js");
+var spotify = new Spotify(keys.spotify);
+
+var requestType = process.argv[2];
+var requestValue = process.argv[3];
+
+var axios = require("axios");
+
+var queryUrl = "https://rest.bandsintown.com/artists/" + requestType + "/events?app_id=codingbootcamp"
+
+axios.get(queryUrl).then(
+    function(response) {
+      console.log(response.data);
+    }
+  );
+  
