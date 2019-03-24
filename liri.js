dc62a7d01a1354672a05623cc316813f5cfaa90e
Rhino <ryanharrisfl@gmail.com>
2019-03-24 15:00:15 -0400

    set up so that The Sign will default if nothing is selected

--- liri.js
+++ liri.js
@@ -1,28 +1,25 @@
 require("dotenv").config();
 var axios = require("axios");
 var fs = require("fs");
-var moment = require('moment');
-var Spotify = require('node-spotify-api');
+var moment = require("moment");
+var Spotify = require("node-spotify-api");
 
 var keys = require("./keys.js");
 
 var requestType = process.argv[2];
+
 var nodeArgs = process.argv;
 
 var artists = "";
-var arr = 0;
+var arr;
 
-console.log(nodeArgs.length);
+arr = artists.length;
+console.log(arr);
 
-function artistsLength () {
-  if (nodeArgs.length === 3) { 
-    nodeArgs = ("The Sign");
-  }
-}
+console.log(artists);
 
 switch (requestType) {
   case "concert-this":
-    artistsLength();
     loopNames();
     bandsInTown();
     break;
@@ -33,20 +30,14 @@ switch (requestType) {
 }
 
 function loopNames() {
-for (var i = 3; i < nodeArgs.length; i++) {
-  if (i > 3 && i < nodeArgs.length) {
-    artists = artists + "+" + nodeArgs[i];
-  } else {
-    artists += nodeArgs[i];
+  for (var i = 3; i < nodeArgs.length; i++) {
+    if (i > 3 && i < nodeArgs.length) {
+      artists = artists + "+" + nodeArgs[i];
+    } else {
+      artists += nodeArgs[i];
+    }
   }
 }
-}
-
-arr = artists.length;
-console.log(arr);
-
-
- console.log(artists);
 
 //Bands In Town Function that retrieves data object
 function bandsInTown() {
@@ -56,31 +47,63 @@ function bandsInTown() {
     "/events?app_id=codingbootcamp";
 
   axios.get(queryUrl).then(function(response) {
-    for (i = 0; i < response.data.length; i++){
+    for (i = 0; i < response.data.length; i++) {
       console.log("Name of the venue: " + response.data[0].venue.name);
       console.log("City: " + response.data[i].venue.city);
-      console.log("Date of the event: " + moment(response.data[i].datetime).format("L"));
-      }
+      console.log(
+        "Date of the event: " + moment(response.data[i].datetime).format("L")
+      );
+    }
   });
 }
 
 //Spotify function that retrieves data object
 function spotifyThisSong() {
-
   var spotify = new Spotify(keys.spotify);
 
-  spotify
-  .search({ type: 'track', query: artists })
-  .then(function(response) {
+  if (artists.length === 0) {
+    spotify
 
-    console.log(JSON.stringify("Artist(s): " + response.tracks.items[0].artists[0].name));
-    console.log(JSON.stringify("Song: " + response.tracks.items[0].name));
-    console.log(JSON.stringify("Album: " + response.tracks.items[0].album.name));
-    console.log(JSON.stringify("Preview: " + response.tracks.items[0].preview_url));
+      .search({ type: "track", query: "The Sign" })
 
-  })
-  .catch(function(err) {
-    console.log(err);
-  });
+      .then(function(response) {
+        console.log(
+          JSON.stringify(
+            "Artist(s): " + response.tracks.items[0].artists[0].name
+          )
+        );
+        console.log(JSON.stringify("Song: " + response.tracks.items[0].name));
+        console.log(
+          JSON.stringify("Album: " + response.tracks.items[0].album.name)
+        );
+        console.log(
+          JSON.stringify("Preview: " + response.tracks.items[0].preview_url)
+        );
+      })
+      .catch(function(err) {
+        console.log(err);
+      });
+  } else {
+    spotify
 
-}       
+      .search({ type: "track", query: artists })
+
+      .then(function(response) {
+        console.log(
+          JSON.stringify(
+            "Artist(s): " + response.tracks.items[0].artists[0].name
+          )
+        );
+        console.log(JSON.stringify("Song: " + response.tracks.items[0].name));
+        console.log(
+          JSON.stringify("Album: " + response.tracks.items[0].album.name)
+        );
+        console.log(
+          JSON.stringify("Preview: " + response.tracks.items[0].preview_url)
+        );
+      })
+      .catch(function(err) {
+        console.log(err);
+      });
+  }
+}
